78547a754b82f9f7e98fdefc97b76ac0aa7a715e
ruka64-dev <192672116+ruka64-dev@users.noreply.github.com>
2025-02-10 23:41:03 +0900

    prettierかけた

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,27 @@
-import * as motion from 'motion/react-client';
+import * as motion from "motion/react-client";
 
 export default function Hero() {
 	return (
 		<>
-			<motion.div className='hello' initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, delay: 0.2 }}>
-				<img src='/icon.webp' draggable='false' loading='eager' fetchPriority='high' width={160} height={160} alt='icon' aria-hidden='true' className='rounded-full' />
-				<h1 className='text-[2rem] font-light text-center'>ruka</h1>
-				<h2 className='text-[1rem] font-light text-center'>I'm a 16y/o developer.</h2>
+			<motion.div
+				className="hello"
+				initial={{ y: 10, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 0.2 }}
+			>
+				<img
+					src="/icon.webp"
+					draggable="false"
+					loading="eager"
+					fetchPriority="high"
+					width={160}
+					height={160}
+					alt="icon"
+					aria-hidden="true"
+					className="rounded-full"
+				/>
+				<h1 className="text-[2rem] font-light text-center">ruka</h1>
+				<h2 className="text-[1rem] font-light text-center">I'm a 16y/o developer.</h2>
 			</motion.div>
 		</>
 	);
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const config = {
-	siteName: 'るかのお部屋',
+	siteName: "るかのお部屋",
 };
