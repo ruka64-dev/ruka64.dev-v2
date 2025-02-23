5aef6af377875a79494f1ad236c683e8d3cb37db
ruka64-dev <192672116+ruka64-dev@users.noreply.github.com>
2025-02-23 16:43:25 +0900

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	output: "server",
 	integrations: [react()],
-	adapter: cloudflare(),
 });
